576879eabbf9b031c3eaa80762a97923095b070f
kp-priest <kp@heykp.ninja>
2025-04-06 11:19:32 -0500

    First coding run for app.js.

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,23 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    const metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    const resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    const result = resultArray[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    const PANEL = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    PANEL.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
 
   });
 }
@@ -25,19 +29,41 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    const samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
+    const sampleData = samples.filter(sampleObj => sampleObj.id == sample);
 
 
     // Get the otu_ids, otu_labels, and sample_values
+    const otu_ids = sampleData[0].otu_ids;
+    const otu_labels = sampleData[0].otu_labels;
+    const sample_values = sampleData[0].sample_values;
 
 
     // Build a Bubble Chart
+    const bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      showlegend: false,
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
 
+    const bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }];
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
